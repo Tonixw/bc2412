87c75abc65d2804cc2d8052f7cee751b70094e55
Tonixw <tonix422@gmail.com>
2024-12-10 16:31:55 +0800

    20241210

--- demo-helloworld/src/Exercise20241210.java
+++ demo-helloworld/src/Exercise20241210.java
@@ -17,6 +17,26 @@ public class Exercise20241210 {
 
     System.out.println(averageScore);
 
+    // byte -> short -> int -> long
+    byte maxByte = 127;
+    byte minByte = -128;
+    
+    // int value -> byte
+    maxByte = (byte)(maxByte - 2); // Java: is it safe? Not safe
+    // byte value + int value -> int value
+    // can we assign int value to byte variable? (downcasting)
+
+    System.out.println(maxByte); // -128, overflow
+
+    int x = maxByte; // Java: is it safe?
+
+    // ! Java: (1)Compile time + (2)Run time
+    // Java file (.java) -> class file (.class):java code -> byte code (~machine)
+      // (1.1) compile fail. for example (missing ; -> syntax error)
+      // (1.2) complie success -> class file
+    // (2) Java Virtual Machine (JVM) -> convert class file to machine code -> execute machine code 
+
+
   }
 }
   
