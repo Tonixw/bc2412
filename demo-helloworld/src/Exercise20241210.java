d4051ed222968107f68f1025941d958c04138ca0
Tonixw <tonix422@gmail.com>
2024-12-10 12:43:51 +0800

    20241210

---
+++ demo-helloworld/src/Exercise20241210.java
@@ -0,0 +1,15 @@
+public class Exercise20241210 {
+  public static void main(String[] args){
+    double priceForApple = 7.3;
+    double priceForOrange = 6.5;
+    int quantityOfApple = 3;
+    int quantityOfOrange = 4;
+
+    double x = 7.3 * 3 + 6.5 * 4;
+
+    System.out.println(x);
+
+  }
+}
+  
+
